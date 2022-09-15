c30835c6d908b8492849e6a94f6b730c8f369605
Carmen Saldivar <Carmen.SaldivarMarin@teck.com>
2022-09-15 09:46:48 -0500

    fix code

--- src/model/traincs.py
+++ src/model/traincs.py
@@ -40,8 +40,8 @@ def split_data(df):
     X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure',
                'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree',
                'Age']].values, df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, 
-                                                        y, test_size=0.30, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, 
+                                                        random_state=0)
     print(X_train, X_test, y_train, y_test)
     return X_train, X_test, y_train, y_test
 
@@ -67,6 +67,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
